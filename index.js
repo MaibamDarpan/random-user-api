b28cdf6b3798b8826c8f7f01cd298d66af42c4e8
MaibamDarpan <dopd2870@gmail.com>
2021-06-13 04:37:54 +0100

    random user name api for fun

---
+++ index.js
@@ -0,0 +1,14 @@
+function randomName(){
+    fetch('https://randomuser.me/api')
+.then(res => res.json())
+.then( data =>{
+    const user = data.results[0];
+    const name = user.name;
+    const userName = `${name.title} ${name.first} ${name.last}`
+    document.getElementById('user-name').innerText = userName;
+})
+}
+randomName();
+setInterval(() => {
+    randomName();
+}, 3000);
